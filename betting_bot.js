cfc6298bb67f8671aa5bda8614fca24ec77820eb
aITy <aITy132@gmail.com>
2014-03-10 19:58:46 +0100

    takted

--- betting_bot.js
+++ betting_bot.js
@@ -149,7 +149,7 @@ nactiData = function() {
 		for (var i = 0; i < bets_content_rows.length; i++) {
 
 			if (bets_content_rows[i].className.indexOf("multiple") == -1) {
-				var betid = bets_content_rows[i].getElementsByClassName("bet-id").innerText;
+				var betid = bets_content_rows[i].getElementsByClassName("bet-id")[0].innerText;
 				var one_bet = bets_content_rows.item(i).childNodes;
 				for (var j = 0; j < one_bet.length; j++) {
 					if (one_bet.item(j).nodeName.toUpperCase() == "TD") {
